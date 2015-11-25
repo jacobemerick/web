4d41dbf406b984a36fa567a5b353a8e1bfb4688d
Jacob Emerick <jpemeric@gmail.com>
2015-11-24 19:14:39 -0700

    Removes some testing code

--- initiator.php
+++ initiator.php
@@ -7,11 +7,10 @@ include_once('utility/Loader.class.inc.php');
 Loader::loadInstance('utility', 'Debugger');
 
 date_default_timezone_set('America/Chicago');
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-// set_error_handler(array('Debugger', 'internal_error'));
-// register_shutdown_function(array('Debugger', 'shutdown'));
+ini_set('display_errors', 0);
+error_reporting(-1);
+set_error_handler(array('Debugger', 'internal_error'));
+register_shutdown_function(array('Debugger', 'shutdown'));
 
 Loader::loadInstance('utility', 'Visitor');
-Loader::loadNew('utility', 'CronController')->activate();
-// Loader::loadInstance('router', 'Router');
+Loader::loadInstance('router', 'Router');
