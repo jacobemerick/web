b45172a1a1a755c3ec716a5cfd959161d9bdc5a5
jacobemerick <jpemeric@gmail.com>
2015-12-26 06:18:52 -0700

    Adds some more sites to direct target approach

---
+++ public/blog/index.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+Loader::loadInstance('utility', 'Debugger');
+
+// sets a few global settings
+date_default_timezone_set('America/Chicago');
+ini_set('display_errors', 0);
+error_reporting(-1);
+set_error_handler(array('Debugger', 'internal_error'));
+register_shutdown_function(array('Debugger', 'shutdown'));
+
+Loader::loadInstance('utility', 'Visitor');
+Loader::loadInstance('router', 'Router');
