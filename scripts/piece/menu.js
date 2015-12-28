3b8e6dbf850ab659ef1949fd92990a76d9f91993
jacobemerick <jpemeric@gmail.com>
2015-12-28 05:33:41 -0700

    Removes deprecated scripts & styles directories

--- scripts/piece/menu.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	$('#menu .drop-li').hover(function() {
-		$('.menu-dropdown').stop(true, true).slideDown();
-	}, function() {
-		$('.menu-dropdown').stop(true, true).slideUp();
-	});
-});
