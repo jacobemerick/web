44b189497507a3cf50e85ea11d641f1600b00f82
jacobemerick <jpemeric@gmail.com>
2015-12-29 12:55:25 -0700

    Removes unused js assets

--- public/js/home.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	$('#main_links a').click(
-		function() {
-			Track.action('home_link', 'click', $(this).attr('href'));});
-	$('#social_links a').click(
-		function() {
-			Track.action('social_link', 'click', $(this).attr('href'));});
-	$('#display_login').click(
-		function() {});});
