4452ca9c3c290657f4c12bfb7bb33bf7407d4ab0
jacobemerick <jpemeric@gmail.com>
2015-12-31 04:59:10 -0700

    Adds some initial Monolog logging

--- public/site/index.php
+++ public/site/index.php
@@ -1,13 +1,31 @@
 <?php
 
+date_default_timezone_set('America/Chicago');
+
 require_once __DIR__ . '/../../bootstrap.php';
 
+// sets up specific logger
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+$formatter = new LineFormatter('[%datetime%] %channel%.%level_name%: %message%');
+
+$handler = new StreamHandler(
+    __DIR__ . '/../../logs/site.log',
+    Logger::DEBUG
+);
+$handler->setFormatter($formatter);
+
+$logger = new Logger('web');
+$logger->pushHandler($handler);
+
+$container['logger'] = $logger;
+$container['logger']->addInfo('Bootstrapping is complete - moving onto routing');
+
+// old setup
 Loader::loadInstance('utility', 'Debugger');
 
-// sets a few global settings
-date_default_timezone_set('America/Chicago');
-ini_set('display_errors', 0);
-error_reporting(-1);
 set_error_handler(array('Debugger', 'internal_error'));
 register_shutdown_function(array('Debugger', 'shutdown'));
 
