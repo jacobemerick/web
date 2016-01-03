a9a3d77739216829100535e85561c33baf869cbd
jacobemerick <jpemeric@gmail.com>
2016-01-03 07:36:02 -0700

    Adds sourcemaps (currently broken), per-env builds, and refresh

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,6 @@
 module.exports = function(grunt) {
+  grunt.config('env', grunt.option('env') || 'dev');
+
   grunt.loadNpmTasks('grunt-bowercopy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
@@ -19,13 +21,17 @@ module.exports = function(grunt) {
       }
     },
     clean: {
-      folder: [
+      build: [
         'bower_components',
         'build'
+      ],
+      refresh: [
+        'public/css/build/*',
+        'public/js/build/*'
       ]
     },
     cssmin: {
-      target: {
+      app: {
         files: {
           'public/css/build/404.css': 'public/css/404.css',
           'public/css/build/503.css': 'public/css/503.css',
@@ -35,20 +41,25 @@ module.exports = function(grunt) {
           ],
           'public/css/build/home.css': 'public/css/home.css',
           'public/css/build/lifestream.css': 'public/css/lifestream.css',
-          'public/css/build/normalize.css': 'build/temp/normalize.css',
           'public/css/build/portfolio.css': 'public/css/portfolio.css',
-          'public/css/build/reset.css': 'build/temp/reset.css',
           'public/css/build/site.css': 'public/css/site.css',
           'public/css/build/waterfalls.css': 'public/css/waterfalls.css'
+        },
+        options: {
+          sourceMap: (grunt.config('env') == 'dev') ? true : false
+        }
+      },
+      vendor: {
+        files: {
+          'public/css/build/normalize.css': 'build/temp/normalize.css',
+          'public/css/build/reset.css': 'build/temp/reset.css'
         }
       }
     },
     uglify: {
-      scripts: {
+      app: {
         files: {
           'public/js/build/imagelightbox.min.js': 'public/js/imagelightbox.js',
-          'public/js/build/jquery.min.js': 'build/temp/jquery.js',
-          'public/js/build/js.cookie.min.js': 'build/temp/js.cookie.js',
           'public/js/build/portfolio.min.js': [
             'public/js/portfolio.js',
             'public/js/imageloader.js'
@@ -57,6 +68,15 @@ module.exports = function(grunt) {
             'public/js/waterfall-overlay.js',
             'public/js/waterfall-map.js'
           ]
+        },
+        options: {
+          sourceMap: (grunt.config('env') == 'dev') ? true : false
+        }
+      },
+      vendor: {
+        files: {
+          'public/js/build/jquery.min.js': 'build/temp/jquery.js',
+          'public/js/build/js.cookie.min.js': 'build/temp/js.cookie.js',
         }
       }
     }
@@ -65,10 +85,11 @@ module.exports = function(grunt) {
   grunt.registerTask(
     'default',
     [
+      'clean:refresh',
       'bowercopy',
       'uglify',
       'cssmin',
-      'clean'
+      'clean:build'
     ]
   );
 };
