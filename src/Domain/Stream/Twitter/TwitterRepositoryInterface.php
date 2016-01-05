0a42653343f84ef2545d3b64ccdf3337bd286f85
jacobemerick <jpemeric@gmail.com>
2016-01-05 07:30:13 -0700

    Code is hard

--- src/Domain/Stream/Twitter/TwitterRepositoryInterface.php
+++ src/Domain/Stream/Twitter/TwitterRepositoryInterface.php
@@ -2,11 +2,8 @@
 
 namespace Jacobemerick\Web\Domain\Stream\Twitter;
 
-use DateTimeInterface;
-
 interface TwitterRepositoryInterface
 {
     public function getTwitterById($id);
-    public function getTwitterByFields(DateTimeInterface $date, $text);
     public function getUnmappedTwitters();
 }
