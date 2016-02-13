88acae049bb461bbd2b19c389668d401f76b3168
jacobemerick <jpemeric@gmail.com>
2016-02-13 05:16:21 -0700

    Removes Goodread cron

--- src/Domain/Stream/Goodread/MysqlGoodreadRepository.php
+++ src/Domain/Stream/Goodread/MysqlGoodreadRepository.php
@@ -19,47 +19,6 @@ class MysqlGoodreadRepository implements GoodreadRepositoryInterface
         $this->connections = $connections;
     }
 
-    /**
-     * @param integer $limit
-     * @param integer $offset
-     *
-     * @return array|false
-     */
-    public function getReviews($limit = null, $offset = 0)
-    {
-        $query = "
-            SELECT `id`, `permalink`, `datetime`
-            FROM `jpemeric_stream`.`goodread`
-            ORDER BY `datetime` DESC";
-        if (!is_null($limit)) {
-          $query .= "
-          LIMIT {$offset}, {$limit}";
-        }
-
-        return $this
-            ->connections
-            ->getRead()
-            ->fetchAll($query);
-    }
-
-    public function getReviewByPermalink($permalink)
-    {
-        $query = "
-            SELECT *
-            FROM `jpemeric_stream`.`goodread`
-            WHERE `permalink` = :permalink
-            LIMIT 1";
-
-        $bindings = [
-            'permalink' => $permalink,
-        ];
-
-        return $this
-            ->connections
-            ->getRead()
-            ->fetchOne($query, $bindings);
-    }
-
     public function getGoodreadsUpdatedSince(DateTime $datetime)
     {
         $query = "
@@ -75,26 +34,5 @@ class MysqlGoodreadRepository implements GoodreadRepositoryInterface
             ->connections
             ->getRead()
             ->fetchAll($query, $bindings);
-    }
-
-    public function insertReview($permalink, $bookId, DateTime $datetime, array $metadata)
-    {
-        $query = "
-            INSERT INTO `jpemeric_stream`.`goodread`
-                (`permalink`, `book_id`, `datetime`, `metadata`)
-            VALUES
-                (:permalink, :book_id, :datetime, :metadata)";
-
-        $bindings = [
-            'permalink' => $permalink,
-            'book_id' => $bookId,
-            'datetime' => $datetime->format('Y-m-d H:i:s'),
-            'metadata' => json_encode($metadata),
-        ];
-
-        return $this
-            ->connections
-            ->getWrite()
-            ->perform($query, $bindings);
     }
 }
