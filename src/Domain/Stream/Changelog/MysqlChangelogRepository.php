b93db5c59371e2211fb709dbfdbd3023065e034e
jacobemerick <jpemeric@gmail.com>
2016-02-15 21:17:20 -0700

    Removes Github cron

--- src/Domain/Stream/Changelog/MysqlChangelogRepository.php
+++ src/Domain/Stream/Changelog/MysqlChangelogRepository.php
@@ -3,7 +3,6 @@
 namespace Jacobemerick\Web\Domain\Stream\Changelog;
 
 use Aura\Sql\ConnectionLocator;
-use DateTime;
 
 class MysqlChangelogRepository implements ChangelogRepositoryInterface
 {
@@ -40,68 +39,5 @@ class MysqlChangelogRepository implements ChangelogRepositoryInterface
             ->connections
             ->getRead()
             ->fetchAll($query);
-    }
-
-    /**
-     * @param string $hash
-     *
-     * @return array|false
-     */
-    public function getChangeByHash($hash)
-    {
-        $query = "
-            SELECT *
-            FROM `jpemeric_stream`.`changelog`
-            WHERE `hash` = :hash
-            LIMIT 1";
-
-        $bindings = [
-            'hash' => $hash,
-        ];
-
-        return $this
-            ->connections
-            ->getRead()
-            ->fetchOne($query, $bindings);
-    }
-
-    /**
-     * @param string   $hash
-     * @param string   $message
-     * @param DateTime $datetime
-     * @param string   $author
-     * @param string   $commit_link
-     *
-     * @return
-     */
-    public function insertChange($hash, $message, DateTime $datetime, $author, $commit_link)
-    {
-        $message_short = $message;
-        $message_short = strtok($message_short, "\n");
-        if (strlen($message_short) > 72) {
-            $message_short = wordwrap($message_short, 65);
-            $message_short = strtok($message_short, "\n");
-            $message_short .= '...';
-        }
-
-        $query = "
-            INSERT INTO `jpemeric_stream`.`changelog`
-                (`hash`, `message`, `message_short`, `datetime`, `author`, `commit_link`)
-            VALUES
-                (:hash, :message, :message_short, :datetime, :author, :commit_link)";
-
-        $bindings = [
-            'hash' => $hash,
-            'message' => $message,
-            'message_short' => $message_short,
-            'datetime' => $datetime->format('Y-m-d H:i:s'),
-            'author' => $author,
-            'commit_link' => $commit_link
-        ];
-
-        return $this
-            ->connections
-            ->getWrite()
-            ->perform($query, $bindings);
     }
 }
--- src/Domain/Stream/Github/GithubRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Jacobemerick\Web\Domain\Stream\Github;
-
-interface GithubRepositoryInterface
-{
-    public function getEvents($limit = null, $offset = 0);
-    public function getEventByEventId($eventId);
-}
