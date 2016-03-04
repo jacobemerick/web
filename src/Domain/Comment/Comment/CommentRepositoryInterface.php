fb94b0225b53cab042d9692daf0a2823f922e536
jacobemerick <jpemeric@gmail.com>
2016-03-04 05:27:45 -0700

    Removes unused repositories for Comments

--- src/Domain/Comment/Comment/CommentRepositoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Jacobemerick\Web\Domain\Comment\Comment;
-
-interface CommentRepositoryInterface
-{
-    public function getActiveCommentsBySite($site, $limit = null, $offset= 0);
-}
