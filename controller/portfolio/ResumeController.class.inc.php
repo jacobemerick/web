1f747822de44ef391de6bb56eddc27ddbe8bd685
jacobemerick <jpemeric@gmail.com>
2016-03-12 15:44:59 -0700

    Loads json resume into resume controller

--- controller/portfolio/ResumeController.class.inc.php
+++ controller/portfolio/ResumeController.class.inc.php
@@ -5,6 +5,8 @@ Loader::load('controller', 'portfolio/DefaultPageController');
 class ResumeController extends DefaultPageController
 {
 
+    protected $resume = 'resume-20160312.json';
+
     protected function set_data()
     {
         $this->set_title("Resume | Jacob Emerick's Portfolio");
@@ -24,7 +26,11 @@ class ResumeController extends DefaultPageController
             'menu' => $this->get_menu(),
             'home_link' => Loader::getRootURL(),
         ]);
-        $this->set_body('body_data', []);
+
+        $resumePath = Loader::getRootURL('portfolio') . "/jsonresume/{$this->resume}";
+        $resume = file_get_contents($resumePath);
+        $resume = json_decode($resume, true);
+        $this->set_body('body_data', $resume);
 
         $this->set_body_view('Page');
     }
