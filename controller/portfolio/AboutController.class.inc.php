30179093d8893247f666442466a2a4a16968a43f
jacobemerick <jpemeric@gmail.com>
2016-03-19 06:53:52 -0700

    Update and style home page of portfolio site

--- controller/portfolio/AboutController.class.inc.php
+++ controller/portfolio/AboutController.class.inc.php
@@ -5,7 +5,7 @@ Loader::load('controller', 'portfolio/DefaultPageController');
 class AboutController extends DefaultPageController
 {
 
-    protected function set_data()
+    protected function set_head_data()
     {
         $this->set_title("Jacob Emerick's Portfolio");
         $this->set_description("Jacob Emerick's Portfolio - collection of programming projects and resume");
@@ -18,15 +18,13 @@ class AboutController extends DefaultPageController
             'agile',
             'freelance',
         ]);
+    }
 
+    protected function set_body_data()
+    {
         $this->set_body('body_view', 'About');
-        $this->set_body('header_data', [
-            'title' => "Jacob Emerick's Portfolio",
-            'menu' => $this->get_menu(),
-            'home_link' => Loader::getRootURL(),
-        ]);
         $this->set_body('body_data', []);
 
-        $this->set_body_view('Page');
+        parent::set_body_data();
     }
 }
