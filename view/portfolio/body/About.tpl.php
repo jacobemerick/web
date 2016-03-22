b5ffd45569b3d1f61e5990ac80d2b3e14f086b47
jacobemerick <jpemeric@gmail.com>
2016-03-21 21:01:25 -0700

    Adds project page

--- view/portfolio/body/About.tpl.php
+++ view/portfolio/body/About.tpl.php
@@ -1,5 +1,5 @@
 <div class="about">
-  <h1>Jacob writes awesome code</h1>
+  <h1>Jacob does code and stuff</h1>
   <img id="jake-picture" src="/image/jacob-emerick.jpg" height="180" width="180" alt="Jacob Emerick" />
   <div class="introduction">
     <p>Howdy! You've found my online portfolio. My name is Jacob Emerick and I am a software engineer who works remotely for Shutterstock. I specialize in backend application architecture and development, though I also have frontend, management, and data science skills as well.</p>
