c9ca9c06df5098aa16842a086dbd7fec387c1999
jacobemerick <jpemeric@gmail.com>
2016-05-16 20:05:25 -0700

    Adds bowties to portfolio site

--- view/portfolio/Page.tpl.php
+++ view/portfolio/Page.tpl.php
@@ -1,13 +1,15 @@
-<div class="portfolio-container">
-  <?= Loader::load('view', 'portfolio/part/Header', $header_data) ?>
-  <div class="window">
-    <?= Loader::load('view', "portfolio/body/{$body_view}", $body_data) ?>
-  </div>
-  <div class="bottom">
-    <div class="inner">
-      <?= Loader::load('view', 'common/SiteLinks', compact('domain_container')) ?>
-      <?= Loader::load('view', 'common/Activity', compact('activity_array', 'domain_container')) ?>
+<div class="bow-tie-container">
+  <div class="portfolio-container">
+    <?= Loader::load('view', 'portfolio/part/Header', $header_data) ?>
+    <div class="window">
+      <?= Loader::load('view', "portfolio/body/{$body_view}", $body_data) ?>
     </div>
+    <div class="bottom">
+      <div class="inner">
+        <?= Loader::load('view', 'common/SiteLinks', compact('domain_container')) ?>
+        <?= Loader::load('view', 'common/Activity', compact('activity_array', 'domain_container')) ?>
+      </div>
+    </div>
+    <?= Loader::load('view', 'common/Footer', $footer) ?>
   </div>
-  <?= Loader::load('view', 'common/Footer', $footer) ?>
 </div>
