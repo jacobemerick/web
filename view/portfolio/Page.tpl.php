b0071f43f37c0bd57aa54cb342679cb48d12bc8a
jacobemerick <jpemeric@gmail.com>
2016-05-17 15:35:55 -0700

    Fixes annoyance with over-scroll

--- view/portfolio/Page.tpl.php
+++ view/portfolio/Page.tpl.php
@@ -1,15 +1,17 @@
+<div class="stripes">
 <div class="bow-tie-container">
-  <div class="portfolio-container">
-    <?= Loader::load('view', 'portfolio/part/Header', $header_data) ?>
-    <div class="window">
-      <?= Loader::load('view', "portfolio/body/{$body_view}", $body_data) ?>
-    </div>
-    <div class="bottom">
-      <div class="inner">
-        <?= Loader::load('view', 'common/SiteLinks', compact('domain_container')) ?>
-        <?= Loader::load('view', 'common/Activity', compact('activity_array', 'domain_container')) ?>
-      </div>
-    </div>
-    <?= Loader::load('view', 'common/Footer', $footer) ?>
+<div class="portfolio-container">
+  <?= Loader::load('view', 'portfolio/part/Header', $header_data) ?>
+  <div class="window">
+    <?= Loader::load('view', "portfolio/body/{$body_view}", $body_data) ?>
   </div>
+  <div class="bottom">
+    <div class="inner">
+      <?= Loader::load('view', 'common/SiteLinks', compact('domain_container')) ?>
+      <?= Loader::load('view', 'common/Activity', compact('activity_array', 'domain_container')) ?>
+    </div>
+  </div>
+  <?= Loader::load('view', 'common/Footer', $footer) ?>
+</div>
+</div>
 </div>
